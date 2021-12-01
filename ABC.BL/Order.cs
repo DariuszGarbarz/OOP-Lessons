44d27acd01dfefdd991350330381e2ca1b73d6a0
DariuszGarbarz <plgarbarda@gmail.com>
2021-12-01 17:43:43 +0100

    Classes added: Order, OrderPosition and Product Sketch of required methods implemented

---
+++ ABC.BL/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public int OrderId { get; private set; }
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public Order Load(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var correct = true;
+
+            if (OrderDate == null)
+            {
+                correct = false;
+            }
+
+            return correct;
+        }
+    }
+}
---
+++ ABC.BL/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            ProduktId = productId;
+        }
+
+        public int ProduktId { get; private set; }
+
+        public Decimal? CurrentPrice { get; set; }
+        public string Description { get; set; }
+        public string ProductName { get; set; }
+
+        public Product Load(int productId)
+        {
+            return new Product();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var correct = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName))
+            {
+                correct = false;
+            }
+
+            return correct;
+        }
+    }
+}
