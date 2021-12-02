d72ccf7b1571d986a2d1c905b74017b2103a817f
DariuszGarbarz <plgarbarda@gmail.com>
2021-12-02 16:03:29 +0100

    Repository function extracted. Added test for Repository Functions

---
+++ ABC.BL/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BL
+{
+    public class OrderRepository
+    {
+
+
+        public Order Load(int orderId)
+        {
+            var order = new Order(orderId);
+            if(orderId == 1)
+            {
+                order.OrderDate = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+
+            return order;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
---
+++ ABC.BL/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace ABC.BL
+{
+    public class ProductRepository
+    {
+
+        public ProductRepository()
+        {
+
+        }
+
+        public Product Load(int productId)
+        {
+            //hard codding fdor tests
+            Product product = new Product(productId);
+            
+            if(productId == 1)
+            {
+                product.ProductName = "rig";
+                product.Description = "rig dla simracera";
+                product.CurrentPrice = 1990.89M;
+            }
+
+            return product;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
